3e0f04a3323b27005e6853e11ae9ab4b6387dec6
Michael Laksa Kharisma <mike_charism@outlook.com>
2025-06-02 19:18:18 +0700

    Initial commit

---
+++ src/main/java/com/juaracoding/smartpro_web/SmartproWebApplication.java
@@ -0,0 +1,13 @@
+package com.juaracoding.smartpro_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartproWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartproWebApplication.class, args);
+	}
+
+}

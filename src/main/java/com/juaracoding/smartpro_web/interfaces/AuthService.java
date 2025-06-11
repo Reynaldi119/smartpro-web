6ab57f0a63ef47067705af98085e6d24b7d3112a
Michael Laksa Kharisma <mike_charism@outlook.com>
2025-06-12 01:59:35 +0700

    Login fixed

--- src/main/java/com/juaracoding/smartpro_web/interfaces/AuthService.java
+++ src/main/java/com/juaracoding/smartpro_web/interfaces/AuthService.java
@@ -1,14 +1,15 @@
 package com.juaracoding.smartpro_web.interfaces;
 
-import com.juaracoding.smartpro_web.dto.validation.LoginDTO;
-import org.springframework.cloud.client.loadbalancer.Response;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import com.juaracoding.smartpro_web.dto.validation.LoginDTO;
 
 @FeignClient(name = "auth-services", url = "http://localhost:8085/auth")
 public interface AuthService {
 
     @PostMapping("/login")
-    public Response<Object> login(@RequestBody LoginDTO loginDTO);
+    public ResponseEntity<Object> login(@RequestBody LoginDTO loginDTO);
 }
